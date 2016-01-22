c0c5d016069d523be06ab618b6c38e748f7e6ccd
VladyslavTaran <ko12@list.ru>
2016-01-22 23:33:58 +0200

    commit all

--- src/utils/ClassString.java
+++ src/utils/ClassString.java
@@ -4,4 +4,20 @@ package utils;
  * Created by Vladislav on 22.01.2016.
  */
 public class ClassString {
+    public static void main(String[] args) {
+        String[] strArr = {"2sss ", "dey6 ", "fff"};
+        System.out.println(create(strArr));
+    }
+
+    public static String create(String[] strArray)
+    {
+        String strRes="";
+
+        for (int i=0; i<strArray.length; i++)
+        {
+            strRes+=strArray[i];
+        }
+
+        return strRes;
+    }
 }
